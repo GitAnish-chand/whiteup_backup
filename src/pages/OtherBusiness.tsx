275f69354a10366a7f8e68284a45b8e690693669
GitAnish-chand <anishchand9984@gmail.com>
2026-01-08 00:57:31 +0530

    sound update done

--- src/pages/OtherBusiness.tsx
+++ src/pages/OtherBusiness.tsx
@@ -1,11 +1,13 @@
+
 import Business1 from "@/components/other_businesses/Business1";
 import { motion } from "framer-motion";
 
 
 const OtherBusiness = () => {
   return (
-    
     <Business1 />
+    
+    
   );
 };
 
