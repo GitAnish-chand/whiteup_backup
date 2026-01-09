25c8db803bf5956747145b7b1dbad07471316c68
GitAnish-chand <anishchand9984@gmail.com>
2026-01-10 03:15:25 +0530

    lag problem fix

--- src/components/sections/VisionSection.tsx
+++ src/components/sections/VisionSection.tsx
@@ -239,7 +239,7 @@ export const VisionSection = () => {
                 style={{ opacity, x }}
                 className={`relative flex items-center mb-16 ${index % 2 === 0
                     ? "justify-start"
-                    : "justify-end"
+                    : "justify-center"
                   }`}
               >
                 {/* 🔹 DOT – CHECKPOINT */}
